62859398bee33be7b00cbbc57f47cc8f85acbf89
0xb337r007 <120017422+0xb337r007@users.noreply.github.com>
2024-08-14 11:10:39 +0200

    chore(compiler): bump min solidity version to 0.8.26 (#30)
    Co-authored-by: Andrea Franz <andrea@gravityblast.com>

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity >=0.8.19 <=0.9.0;
+pragma solidity >=0.8.26 <=0.9.0;
 
 import { Foo } from "../src/Foo.sol";
 import { BaseScript } from "./Base.s.sol";
